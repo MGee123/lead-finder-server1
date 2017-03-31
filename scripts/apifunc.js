479313b642442c55342e04df7e7aafa2bf4636fb
leknaat <wishfire@gmail.com>
2017-03-31 14:56:39 +0100

    Passing lint, forEach loop done with array. Will need to later put this on a delay.

--- scripts/apifunc.js
+++ scripts/apifunc.js
@@ -1,35 +1,17 @@
-var request = require('superagent-es6-promise');
+import request from 'superagent-es6-promise'
 
-var apiURLs = ["https://extraction.import.io/query/extractor/9fe09a64-bf12-4f8e-8738-26b08fac992b?_apikey=ec60cbd9df2e415096febae644cc6c1d443c3f192dc8f1e0a223e61d83eca2cddc9d6da176236fc163e3c913ecd0e6d4cb8a5cf33dc1040d839e4c9a709af2b1723fa70d2151ee1bc7441a9b5cdc18f8&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fforhire%2Fsearch%3Fq%3D%255Bhiring%255D%2Breact%26restrict_sr%3Don%26sort%3Dnew%26t%3Dall",
-"https://extraction.import.io/query/extractor/16d7107e-18d8-4ba2-bcee-f04e369f702a?_apikey=ec60cbd9df2e415096febae644cc6c1d443c3f192dc8f1e0a223e61d83eca2cddc9d6da176236fc163e3c913ecd0e6d4cb8a5cf33dc1040d839e4c9a709af2b1723fa70d2151ee1bc7441a9b5cdc18f8&url=https%3A%2F%2Fwww.upwork.com%2Fo%2Fjobs%2Fbrowse%2F%3Fq%3Dreact"]
+/* eslint-disable */
+const apiURLs = ['https://extraction.import.io/query/extractor/95f6d381-2e7f-4238-affc-0ffd1fea1381?_apikey=11baff8f0228467699c5206895d888dc53f60d97425b95469c591449ca1941871e9fb8396574c617e6ec36e457f8c0e17cc41df53feb67d54e608105c65f820f8b1415a2bde0e6cdb22ff6b1e4d744eb&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fforhire%2Fsearch%3Fq%3D%255Bhiring%255D%2Breact%26restrict_sr%3Don%26sort%3Dnew%26t%3Dall']
+/* eslint-enable */
 
-
-/* old code before forEach loop, for reference:
-   (remove later)
-var apiData = [];
-for (var i = 0; i < apiURLs.length; i++) {
-    var url = apiURLs[i];
-    function apiCall () {
-        request.get(url)
-            .then(function(res) {
-                console.log(res);
+apiURLs.forEach((url) => {
+  request.get(url)
+            .then((res) => {
+              console.log(res)
             })
-            .catch(function(error) {
-                console.log(error);
-            });
-    }
-    apiData.push(apiCall());
-}*/
-
-var apiData=[];
-var apiCall = apiURLs.forEach(function(url) {
-        request.get(url)
-            .then(function(res) {
-                console.log(res);
+            .catch((error) => {
+              console.log(error)
             })
-            .catch(function(error) {
-                console.log(error);
-            });
-        },
-    apiData.push(apiCall),
+},
+
 )
