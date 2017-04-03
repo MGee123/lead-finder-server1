56b3702674756da7148e4e56ee2b593a02c882e7
leknaat <wishfire@gmail.com>
2017-04-03 12:32:39 +0100

    Made changes as requested. 'initialize' was changed to 'initialiseFirebase', config changes, and import changes.

--- scripts/initializeFirebase.js
+++ scripts/initializeFirebase.js
@@ -1,15 +1,6 @@
-import * as firebase from 'firebase'
+import firebase from 'firebase'
+import config from './config'
 
-// Initialize Firebase
-export const config = {
-  apiKey: 'AIzaSyBMkg8RTS8ddq6ySjsU_ElafkdoriCZfEA',
-  authDomain: 'lead-finder-dev.firebaseapp.com',
-  databaseURL: 'https://lead-finder-dev.firebaseio.com',
-  projectId: 'lead-finder-dev',
-  storageBucket: 'lead-finder-dev.appspot.com',
-  messagingSenderId: '370651581120'
-}
-
-export const initialize = () => { firebase.initializeApp(config) }
-export default initialize
+export const initializeFirebase = () => { firebase.initializeApp(config) }
+export default initializeFirebase
 
